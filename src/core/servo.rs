bb0311b63bcb0a4a9173fcd03ef7388d54e7e763
Braden Everson <bradeneverson@gmail.com>
2024-03-12 18:21:10 -0500

    Create utility servo fns

---
+++ src/core/servo.rs
@@ -0,0 +1,28 @@
+use std::{error::Error, time::Duration, thread};
+
+use rppal::gpio::OutputPin;
+
+
+const PERIOD_MS: u64 = 20;
+const PULSE_MIN_US: u64 = 1200;
+const PULSE_NEUTRAL_US: u64 = 1500;
+const PULSE_MAX_US: u64 = 1800;
+
+fn deg_to_pulse(deg: u8) -> u64 {
+    ((11.0 / 3.0) * (deg as f32) + 1200.0) as u64
+}
+
+fn move_servo(servo: &mut OutputPin, to: u64) -> Result<(), Box<dyn Error>> {
+    for pulse in (PULSE_MIN_US..=to).step_by(10) {
+        servo.set_pwm(
+            Duration::from_millis(PERIOD_MS),
+            Duration::from_micros(pulse),
+            )?;
+        thread::sleep(Duration::from_millis(20));
+    }
+    Ok(())
+}
+
+pub fn move_to_deg(servo: &mut OutputPin, to_deg: u8) -> Result<(), Box<dyn Error>> {
+    move_servo(servo, deg_to_pulse(to_deg))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,15 @@
 use std::error::Error;
-use std::thread;
-use std::time::Duration;
 
-use rppal::gpio::{Gpio, OutputPin};
+use eden::core::servo::move_to_deg;
+use rppal::gpio::Gpio;
 
 const SERVO: u8 = 23;
-
-// Servo configuration. Change these values based on your servo's verified safe
-// minimum and maximum values.
-//
-// Period: 20 ms (50 Hz). Pulse width: min. 1200 µs, neutral 1500 µs, max. 1800 µs.
-const PERIOD_MS: u64 = 20;
-const PULSE_MIN_US: u64 = 1200;
-const PULSE_NEUTRAL_US: u64 = 1500;
-const PULSE_MAX_US: u64 = 1800;
-
-fn move_servo(servo: &mut OutputPin, to: u64) -> Result<(), Box<dyn Error>> {
-    for pulse in (PULSE_MIN_US..=to).step_by(10) {
-        servo.set_pwm(
-            Duration::from_millis(PERIOD_MS),
-            Duration::from_micros(pulse),
-            )?;
-        thread::sleep(Duration::from_millis(20));
-    }
-    Ok(())
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut pin = Gpio::new()?.get(SERVO)?.into_output();
 
-    move_servo(&mut pin, PULSE_NEUTRAL_US)?;
-    // Rotate the servo to its neutral (center) position in small steps.
+    move_to_deg(&mut pin, 180)?;
+    move_to_deg(&mut pin, 90)?;
 
     Ok(())
 }
